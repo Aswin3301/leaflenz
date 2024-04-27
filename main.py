4923407b2184b16fea3c3fa4e0a6b2c2c17c5ea6
aswin2321 <168284925+aswin2321@users.noreply.github.com>
2024-04-27 19:55:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+
+classes = ['Early Stage', 'Mid Stage', 'Last Stage']
+
+st.title("Disease Prediction")
+
+@st.cache_resource
+def load_model():
+    return tf.keras.models.load_model("Data_trained_disease_model.h5")
+
+def preprocess_image(img):
+    img = image.load_img(img, target_size=(150, 150))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array /= 255.
+    return img_array
+
+
+model = load_model()
+
+img = st.file_uploader(label="Image File", type=(['png', 'jpg']))
+
+
+if img:
+    st.image(img)
+    img_arr = preprocess_image(img)
+    print(img_arr)
+    pred = model.predict(img_arr)
+    pred_class_index = np.argmax(pred)
+    st.success(f"Predicted class probabilities: {pred}")
+    st.success("Predicted class: " + classes[pred_class_index])
