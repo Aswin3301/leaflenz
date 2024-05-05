4547792b8f9d3bb749804140f08f0113072bfc27
Kishor <kishorramanna5@gmail.com>
2024-05-05 18:30:36 +0530

    Added Content

--- main.py
+++ main.py
@@ -7,6 +7,55 @@ from tensorflow.keras.preprocessing import image
 
 classes = ['Early Stage', 'Mid Stage', 'Last Stage']
 
+class_content = {
+    'Early Stage': """EARLY STAGE:
+• Sanitation: Trimming of the infected parts to stop the spread.
+• Pesticides application: Apply every 7-14 days of interval.
+Chemical treatment suggestions:
+1. Copper oxychloride (synthetic chemical): 3g/L water.
+2. Diphenaconozole (synthetic chemical): 0.5ml /L water.
+3. Allium cepa extract (botanical chemical): 10ml /L water.
+
+Important considerations during chemical treatment:
+• Weather conditions: Ensure no rain for at least 24 hours for chemical adherence to the plant surfaces.
+• Plant growth stages: Pay special attention when periods of rapid growth or when fruit is setting.
+• Local regulations and recommendations: Consult local agricultural authorities for tailored advice and adhere to guidelines.
+• Product label: Always follow the product label for concentration, frequency and safety precautions.
+""",
+    'Mid-Stage': """MID-STAGE:
+• MID STAGE:
+• Avoid over-irrigation: Be careful with the splashing of water. Drip or micro irrigation sprinkler is recommended.
+• Pesticides application: Apply every 7days of interval.
+Chemical treatment suggestions:
+1. Copper oxychloride (synthetic chemical): 3g/L water.
+2. Diphenaconozole (synthetic chemical): 0.5ml /L water.
+3. Allium cepa extract (botanical chemical): 10ml /L water.
+
+Important considerations during chemical treatment:
+• Weather conditions: Ensure no rain for at least 24 hours for chemical adherence to the plant surfaces.
+• Plant growth stages: Pay special attention when periods of rapid growth or when fruit is setting.
+• Local regulations and recommendations: Consult local agricultural authorities for tailored advice and adhere to guidelines.
+• Product label: Always follow the product label for concentration, frequency and safety precautions.
+""",
+    'Last Stage': """LAST STAGE:
+• LATE STAGE:
+• Tree removal: To prevent the spread to nearby healthy trees.
+• Pesticides application: Apply every 5-7 days of interval.
+
+Chemical treatment suggestions:
+1. Copper oxychloride (synthetic chemical): 3g/L water.
+2. Diphenaconozole (synthetic chemical): 0.5ml /L water.
+3. Allium cepa extract (botanical chemical): 10ml /L water.
+
+Important considerations during chemical treatment:
+• Weather conditions: Ensure no rain for at least 24 hours for chemical adherence to the plant surfaces.
+• Plant growth stages: Pay special attention when periods of rapid growth or when fruit is setting.
+• Local regulations and recommendations: Consult local agricultural authorities for tailored advice and adhere to guidelines.
+• Product label: Always follow the product label for concentration, frequency and safety precautions.
+"""
+}
+
+
 st.title("Plant Stage Prediction")
 
 @st.cache_resource
@@ -42,5 +91,10 @@ if img:
     print(img_arr)
     pred = model.predict(img_arr)
     pred_class_index = np.argmax(pred)
+    pred_class_label = classes[pred_class_index]
     st.success(f"Predicted class probabilities: {pred}")
-    st.success("Predicted class: " + classes[pred_class_index])
+    st.success("Predicted class: " + pred_class_label)
+    if pred_class_label in class_content:
+        st.success(class_content[pred_class_label])
+    else:
+        st.success("No content available for predicted classs:", pred_class_label)
